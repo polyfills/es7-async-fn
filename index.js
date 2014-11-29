3ddf3c5a565703e321ab13fd1a1b161f5586c70d
Jonathan Ong <jonathanrichardong@gmail.com>
2014-11-29 00:25:26 -0800

    remove through

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 var recast = require("recast");
 var types = recast.types;
-var through = require("through");
 var esprima = require("esprima-fb");
 var visitor = require("./visitor");
 
 // Make sure that this esprima can parse async functions.
 esprima.parse("async function test() {}");
-
-function exports() {
-  var data = [];
-  return through(write, end);
-
-  function write(buf) {
-    data.push(buf);
-  }
-
-  function end() {
-    this.queue(compile(data.join("")).code);
-    this.queue(null);
-  }
-}
 
 function compile(source, options) {
   options = options || {};
@@ -38,6 +23,5 @@ function transform(ast) {
   return recast.visit(ast, visitor);
 }
 
-module.exports = exports;
 exports.compile = compile;
 exports.transform = transform;
